235fd0378f0d9d1a298542ce564fcc0729d87847
Justin Zhong <email@justinzhong.com>
2019-02-11 10:46:47 +1100

    feat: initial runnable demo

---
+++ Demo.cs
@@ -0,0 +1,105 @@
+﻿using Microsoft.Extensions.Caching.Memory;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+    public class Demo
+    {
+        private readonly static Random _rand = new Random();
+        private readonly ConcurrentDictionary<string, Lazy<int>> _concurrentDictionary;
+        private readonly Dictionary<string, Lazy<int>> _dictionary;
+        private readonly IMemoryCache _memoryCache;
+
+        public Demo(IMemoryCache memoryCache)
+        {
+            _concurrentDictionary = new ConcurrentDictionary<string, Lazy<int>>();
+            _dictionary = new Dictionary<string, Lazy<int>>();
+            _memoryCache = memoryCache ?? throw new ArgumentNullException(nameof(memoryCache));
+        }
+
+        public async Task Run(int runs)
+        {
+            var stopWatch = new Stopwatch();
+            stopWatch.Start();
+
+            Console.WriteLine($"# MemoryCache {runs} x GetOrAdd");
+            await RunTest(WithMemoryCache, runs);
+
+            Console.WriteLine("\r\n--\r\n");
+
+            Console.WriteLine($"# ConcurrentDictionary {runs} x GetOrCreate");
+            await RunTest(WithConcurrentDictionary, runs);
+
+            stopWatch.Stop();
+            TimeSpan ts = stopWatch.Elapsed;
+
+            // Format and display the TimeSpan value.
+            string elapsedTime = string.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+                ts.Hours, ts.Minutes, ts.Seconds,
+                ts.Milliseconds / 10);
+            Console.WriteLine("RunTime " + elapsedTime);
+        }
+
+        private async Task RunTest(Func<int> scenario, int runs)
+        {
+            var result = await Task.WhenAll(Enumerable.Range(1, runs).Select(run => Task.Run(() => scenario())));
+            var distinctResult = result.Distinct().Select(r => r.ToString()).ToArray();
+
+            Console.WriteLine($"{(distinctResult.Length == 1 ? "consistent output" : "ambiguous output")}: [{string.Join(", ", distinctResult)}]");
+        }
+
+        private int WithConcurrentDictionary()
+        {
+            var factory = new Lazy<int>(() =>
+            {
+                var rand = _rand.Next();
+
+                Console.WriteLine($"[thread {Thread.CurrentThread.ManagedThreadId}]: Invoked factory with random value: {rand}");
+
+                return rand;
+            });
+
+            var ret = _concurrentDictionary.GetOrAdd("key", (entry) =>
+            {
+                Console.WriteLine($"[thread {Thread.CurrentThread.ManagedThreadId}]: Cache key miss");
+                Task.Delay(10);
+
+                return factory;
+            });
+
+            Thread.Sleep(5);
+
+            return ret.Value;
+        }
+
+        private int WithMemoryCache()
+        {
+            var factory = new Lazy<int>(() =>
+            {
+                var rand = _rand.Next();
+
+                Console.WriteLine($"[thread {Thread.CurrentThread.ManagedThreadId}]: Invoked factory with random value: {rand}");
+
+                return rand;
+            });
+
+            var ret = _memoryCache.GetOrCreate("key", (entry) =>
+            {
+                Console.WriteLine($"[thread {Thread.CurrentThread.ManagedThreadId}]: Cache key miss");
+                Task.Delay(10);
+
+                return factory;
+            });
+
+            Thread.Sleep(5);
+
+            return ret.Value;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var runs = 100000;
+
+            if (args.Length == 1)
+            {
+                int.TryParse(args[0], out runs);
+            }
+
+            var services = new ServiceCollection();
+            services.AddMemoryCache();
+            services.AddTransient<Demo>();
+
+            var serviceProvider = services.BuildServiceProvider();
+            serviceProvider.GetService<Demo>().Run(runs).GetAwaiter().GetResult();
+
+            Console.WriteLine("Press any key to end this program");
+            Console.ReadKey();
+        }
+    }
+}
